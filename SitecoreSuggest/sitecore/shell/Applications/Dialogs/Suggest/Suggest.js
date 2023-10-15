5d1f72a512075a24904060a64e4b55fcf39b9acc
Kristoffer Kjeldby <Kristoffer.Kjeldby@isobar.com>
2023-10-15 15:04:52 +0200

    Update UI

--- SitecoreSuggest/sitecore/shell/Applications/Dialogs/Suggest/Suggest.js
+++ SitecoreSuggest/sitecore/shell/Applications/Dialogs/Suggest/Suggest.js
@@ -1,1 +1,38 @@
-﻿
+﻿window.addEventListener('load', sitecoreSuggest_onLoad);
+
+function sitecoreSuggest_onLoad(event) {
+
+    let promptFieldIdComboBox = document.getElementById("PromptFieldIdComboBox");
+    let promptEdit = document.getElementById("PromptEdit");
+    let generateButton = document.getElementById("GenerateButton");
+    let generateMoreButton = document.getElementById("GenerateMoreButton");
+    let appendButton = document.getElementById("AppendButton");
+    let insertButton = document.getElementById("InsertButton");
+
+    function enable() {
+        generateMoreButton.disabled = false;
+        appendButton.disabled = false;
+        insertButton.disabled = false;
+    }
+
+    function disable() {
+        generateMoreButton.disabled = true;
+        appendButton.disabled = true;
+        insertButton.disabled = true;
+    }
+
+    promptEdit.addEventListener("input", function () {
+        promptFieldIdComboBox.disabled = promptEdit.value.length > 0;
+    });
+
+    generateButton.addEventListener("click", function () {
+        console.log("GenerateButton click");
+        enable();
+    });
+
+    generateMoreButton.addEventListener("click", function () {
+        console.log("GenerateMoreButton click");
+    });
+
+    disable();
+}
